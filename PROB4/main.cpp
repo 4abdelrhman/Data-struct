7786f002c829ee50c6d74208b18e9d9850831a4e
Abdelrhman Ahmed <157153388+4abdelrhman@users.noreply.github.com>
2025-03-22 02:33:58 +0200

    Update main.cpp

--- PROB4/main.cpp
+++ PROB4/main.cpp
@@ -1,17 +1,16 @@
 #include"SortingSystem.h"
 #include<iostream>
+#include<fstream>
 using namespace std;
 
 int main() {
+    ifstream file("test.txt");
+    if(!file) cout << "Error while opening the file Try again. ";
     int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
+    file >> n;
+    cout << "Data size = " << n  << endl;
     SortingSystem<int> sorter(n);
     sorter.showMenu();
+    file.close();
     return 0;
 }
-
-
-
-//g++ -o sorting main.cpp 
