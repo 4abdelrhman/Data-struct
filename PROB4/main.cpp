249d41d8ee2029aecbd03f9c28abf6141268e8c3
Abdelrhman Ahmed <157153388+4abdelrhman@users.noreply.github.com>
2025-03-22 21:22:43 +0200

    Update main.cpp

--- PROB4/main.cpp
+++ PROB4/main.cpp
@@ -5,12 +5,29 @@ using namespace std;
 
 int main() {
     ifstream file("test.txt");
+    int n, choice;
+
     if(!file) cout << "Error while opening the file Try again. ";
-    int n;
+    cout << "1- Int\n"; cout <<"2- float\n"; cout << "3- string\n";
+    cout <<"Enter the type of the data: ";
+    cin >> choice;
     file >> n;
     cout << "Data size = " << n  << endl;
-    SortingSystem<int> sorter(n);
-    sorter.showMenu();
+    if(choice == 1){
+        SortingSystem<int> sorter(n);
+        sorter.showMenu();
+    }else if(choice == 2){
+        SortingSystem<float> sorter(n);
+        sorter.showMenu();
+    }
+    else if(choice == 3){
+        SortingSystem<string> sorter(n);
+        sorter.showMenu();
+        
+    }
+    else{
+        cout <<"Invalid Choice.";
+    }
     file.close();
     return 0;
 }
