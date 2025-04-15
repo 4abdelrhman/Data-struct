95eb3f8236d53de033d8bc65e28cc52319d46430
Abdelrhman Ahmed <157153388+4abdelrhman@users.noreply.github.com>
2025-04-15 09:37:49 +0200

    Update main.cpp

--- PROB4/main.cpp
+++ PROB4/main.cpp
@@ -4,30 +4,43 @@
 using namespace std;
 
 int main() {
-    ifstream file("test.txt");
+    ifstream fileINT("fileINT.txt");
+    ifstream fileDouble("fileDouble.txt");
+    ifstream fileString("fileString.txt");
     int n, choice;
 
-    if(!file) cout << "Error while opening the file Try again. ";
-    cout << "1- Int\n"; cout <<"2- float\n"; cout << "3- string\n";
+    if(!fileINT) cout << "Error while opening the INT file Try again. ";
+    if(!fileDouble) cout << "Error while opening the Double file Try again. ";
+    if(!fileString) cout << "Error while opening the String file Try again. ";
+
+    cout << "1- Int\n"; cout <<"2- double\n"; cout << "3- string\n";
     cout <<"Enter the type of the data: ";
     cin >> choice;
-    file >> n;
-    cout << "Data size = " << n  << endl;
+
+    
     if(choice == 1){
+        fileINT >> n;
+        cout << "Data size = " << n  << endl;
         SortingSystem<int> sorter(n);
         sorter.showMenu();
-    }else if(choice == 2){
-        SortingSystem<float> sorter(n);
+    }
+    else if(choice == 2){
+        fileDouble >> n;
+        cout << "Data size = " << n  << endl;
+        SortingSystem<double> sorter(n);
         sorter.showMenu();
     }
     else if(choice == 3){
+        fileString >> n;
+        cout << "Data size = " << n  << endl;
         SortingSystem<string> sorter(n);
-        sorter.showMenu();
-        
+        sorter.showMenu(); 
     }
     else{
         cout <<"Invalid Choice.";
     }
-    file.close();
+    fileINT.close();
+    fileDouble.close();
+    fileString.close();
     return 0;
 }
